2dd9ae3cc428117108b7c2262b9b7f512657adaf
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-04 22:20:47 -0300

    using interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 
 const url: string = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  const todo = response.data;
+  const todo = response.data as Todo;
 
   const id = todo.id;
   const title = todo.title;
