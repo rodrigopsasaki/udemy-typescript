97f4f2022ac2a5758910346472586b8130817d46
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-05 14:48:32 -0300

    classes and inheritance

---
+++ features/classes.ts
@@ -0,0 +1,15 @@
+class Vehicle {
+  drive(): void {
+    console.log('chugga chugga');
+  }
+
+  honk(): void {
+    console.log('beep');
+  }
+}
+
+class Car extends Vehicle {}
+
+const car = new Car();
+car.drive();
+car.honk();
