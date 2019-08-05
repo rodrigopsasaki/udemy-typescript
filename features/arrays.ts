af29415af41bebdfc0c5f17375ff1f6e0f9ec8de
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-05 10:35:45 -0300

    typed arrays

---
+++ features/arrays.ts
@@ -0,0 +1,8 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [['f150'], ['corolla'], ['impala']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
