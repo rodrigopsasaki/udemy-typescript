33d6189699dfcf9a40a6c8be7020bbf69ff85d3d
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-14 23:11:48 -0300

    adding csv reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,8 +1,22 @@
-import fs from 'fs';
+import { CsvFileReader } from './csv_file_reader';
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-console.log(matches);
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
