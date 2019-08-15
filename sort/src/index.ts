2708d1c6f7ddc203fda5fa430f660d2aa8595354
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-14 21:58:25 -0300

    refactoring with abstract classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,21 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './numbers_collection';
 import { CharactersCollection } from './characters_collection';
+import { LinkedList } from './linked_list';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const numbersSorter = new Sorter(numbersCollection);
-numbersSorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('buqpeyqkj');
-const characterSorter = new Sorter(charactersCollection);
-characterSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(4);
+linkedList.add(3);
+linkedList.add(1);
+linkedList.add(10);
+linkedList.add(-9);
+linkedList.sort();
+linkedList.print();
