dabfc1c371b7c11594b7a547ce7b5e9b05e0fbcc
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-16 17:18:00 -0300

    new structure

--- stats/src/match_reader.ts
+++ stats/src/match_reader.ts
@@ -1,19 +1,31 @@
-import { CsvFileReader } from './csv_file_reader';
 import { stringToDate } from './utils';
 import { MatchResult } from './match_result';
+import { MatchData } from './match_data';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
 
-export class MatchReader extends CsvFileReader<MatchData> {
-  mapRow(row: string[]): MatchData {
-    return [
-      stringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          stringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
   }
 }
