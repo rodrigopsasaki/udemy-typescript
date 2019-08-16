97378c9475ed877b4688ccaf07876170d7698b4c
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2019-08-16 17:30:26 -0300

    adding html report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,9 +1,14 @@
 import { CsvFileReader } from './csv_file_reader';
 import { MatchReader } from './match_reader';
-import { MatchResult } from './match_result';
+import { ConsoleReport } from './reportTargets/console_report';
+import { WinsAnalysis } from './analyers/wins_analysis';
+import { Summary } from './summary';
+import { HtmlReport } from './reportTargets/html_report';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+
+summary.buildAndPrintReport(matchReader.matches);
